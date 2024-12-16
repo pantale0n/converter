d539fd3c97cfc263629ac68fc7d83d8d483b8142
Pontaleon <oi.kostyuk@mail.ru>
2024-12-16 21:25:37 +0300

    Добавил ответ на домашнее задание №2 , первый вариант

--- main.go
+++ main.go
@@ -5,7 +5,18 @@ import "fmt"
 func main() {
 	const usdToEur = 0.94
 	const usdToRub = 100.0
-	euro := 1000.0
+	euro := getUserInput()
 	rub := euro / usdToEur * usdToRub
 	fmt.Printf("Сумма в рублях: %.2f", rub)
+}
+
+func getUserInput() float64 {
+	var euro float64
+	fmt.Print("Введите сумму для конвертации: ")
+	fmt.Scan(&euro)
+	return euro
+}
+
+func convert(usNum float64, val1 string, val2 string) float64 {
+
 }

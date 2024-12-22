b61fa35553e526c5e72fa6289f2fb7a83f4b8be8
Pontaleon <oi.kostyuk@mail.ru>
2024-12-22 10:33:27 +0300

    Убрал избыточные условия, внес исправления условия проверки - rud на rub

--- main.go
+++ main.go
@@ -3,20 +3,56 @@ package main
 import "fmt"
 
 func main() {
+
+	inValConvert, cntConvert, outValConvert := getUserInput()
+	calculateConvert(inValConvert, cntConvert, outValConvert)
+}
+
+func getUserInput() (string, float64, string) {
+	var inValConvert string
+	var outValConvert string
+	var cntConvert float64
+	fmt.Println("Какую валюту вы хотите обменять (rub/eur/usd)? ")
+	fmt.Scan(&inValConvert)
+	fmt.Println("Укажите сумму обмена: ")
+	fmt.Scan(&cntConvert)
+	switch {
+	case inValConvert == "rub":
+		fmt.Println("Укажите какую валюту вы хотите купить (eur/usd): ")
+		fmt.Scan(&outValConvert)
+	case inValConvert == "eur":
+		fmt.Println("Укажите какую валюту вы хотите купить (rub/usd): ")
+		fmt.Scan(&outValConvert)
+	case inValConvert == "usd":
+		fmt.Println("Укажите какую валюту вы хотите купить (eur/rub): ")
+		fmt.Scan(&outValConvert)
+	}
+	return inValConvert, cntConvert, outValConvert
+}
+
+func calculateConvert(inValConvert string, cntConvert float64, outValConvert string) {
 	const usdToEur = 0.94
 	const usdToRub = 100.0
-	euro := getUserInput()
-	rub := euro / usdToEur * usdToRub
-	fmt.Printf("Сумма в рублях: %.2f", rub)
-}
+	const eurToRub = 110.0
 
-func getUserInput() float64 {
-	var euro float64
-	fmt.Print("Введите сумму для конвертации: ")
-	fmt.Scan(&euro)
-	return euro
-}
-
-func convert(usNum float64, val1 string, val2 string) float64 {
-
+	switch {
+	case inValConvert == "usd" && outValConvert == "eur":
+		resultConvert := cntConvert * usdToEur
+		fmt.Printf("Результат конвертации USD в EUR: %.2f ", resultConvert)
+	case inValConvert == "eur" && outValConvert == "usd":
+		resultConvert := cntConvert / usdToEur
+		fmt.Printf("Результат конвертации EUR в USD: %.2f ", resultConvert)
+	case inValConvert == "usd" && outValConvert == "rub":
+		resultConvert := cntConvert * usdToRub
+		fmt.Printf("Результат конвертации USD в RUB: %.2f ", resultConvert)
+	case inValConvert == "rub" && outValConvert == "usd":
+		resultConvert := cntConvert / usdToRub
+		fmt.Printf("Результат конвертации RUB в USD: %.2f ", resultConvert)
+	case inValConvert == "eur" && outValConvert == "rub":
+		resultConvert := cntConvert * eurToRub
+		fmt.Printf("Результат конвертации EUR в RUB: %.2f ", resultConvert)
+	case inValConvert == "rub" && outValConvert == "eur":
+		resultConvert := cntConvert / eurToRub
+		fmt.Printf("Результат конвертации RUB в EUR: %.2f ", resultConvert)
+	}
 }
